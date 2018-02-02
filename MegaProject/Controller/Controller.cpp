19a50cc160fb29fb6ed18671941a64fbc489db71
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2018-02-02 12:11:33 -0700

    made it get the largest and smallest data set

--- MegaProject/Controller/Controller.cpp
+++ MegaProject/Controller/Controller.cpp
@@ -12,20 +12,49 @@ using namespace std;
 
 void Controller:: start()
 {
-    Timer codeTimer;
-    codeTimer.startTimer();
-    cout << "look on the screen" << endl;
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    codeTimer.resetTimer();
-    codeTimer.startTimer();
     
+    findMinAndMax();
+//    Timer codeTimer;
+//    codeTimer.startTimer();
+//    cout << "look on the screen" << endl;
+//    codeTimer.stopTimer();
+//    codeTimer.displayInformation();
+//    codeTimer.resetTimer();
+//    codeTimer.startTimer();
+//
+//    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/nuff6998/Documents/SwiftCode/MegaProject/MegaProject/Data/crime.csv");
+//
+//    for (int i = 200; i < 215; i++)
+//    {
+//        cout << i << " is " << myData[i] << endl;
+//    }
+//    codeTimer.stopTimer();
+//    codeTimer.displayInformation();
+}
+
+void Controller :: findMinAndMax()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/nuff6998/Documents/SwiftCode/MegaProject/MegaProject/Data/crime.csv");
     
-    for (int i = 200; i < 215; i++)
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 1; index < myData.size(); index++)
     {
-        cout << i << " is " << myData[i] << endl;
+     if(myData [minIndex] > myData [index])
+     {
+         minIndex = index;
     }
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
+        if (myData [maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
+       
+    }
+    cout << "The smallest crime stat is at " << minIndex << " and it is " << myData[minIndex] << endl;
+    cout << "the largest crime stat is at " << maxIndex << " and it is " << myData[maxIndex] << endl;
+    searchTimer.stopTimer();
+    searchTimer.displayInformation();
 }
---
+++ MegaProject/Model/Array.hpp
@@ -0,0 +1,15 @@
+//
+//  Array.hpp
+//  MegaProject
+//
+//  Created by Uffens, Noah on 2/2/18.
+//  Copyright © 2018 Uffens, Noah. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <assert.h>
+#include <iostream>
+
+#endif /* Array_hpp */
