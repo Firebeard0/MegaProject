4e140afa9b9c844ee32d8b2b5c84b6a9331fc48a
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2018-02-08 12:57:38 -0700

    made list header file

---
+++ MegaProject/Model/List.h
@@ -0,0 +1,14 @@
+//
+//  List.h
+//  MegaProject
+//
+//  Created by Uffens, Noah on 2/8/18.
+//  Copyright © 2018 Uffens, Noah. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+#include <assert.h>
+
+
+#endif /* List_h */
